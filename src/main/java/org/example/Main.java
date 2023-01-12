553f447140f424b8cb8b3f936c2b0016c425c855
Piotr Przybylak <piotr.przybylak@gmail.com>
2023-01-12 17:29:57 +0100

    You have to store which languages flight attendants speak from the relevant ones (English, German, Spanish, Polish).

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,22 +2,25 @@ package org.example;
 
 import org.example.airline.Attendant;
 import org.example.airline.Flight;
-import org.example.airline.Language;
 import org.example.airline.Pilot;
 
 import java.util.List;
+import java.util.Set;
+
+import static org.example.airline.Language.ENGLISH;
+import static org.example.airline.Language.GERMAN;
 
 public class Main {
     public static void main(String[] args) {
 
-        Flight flight = new Flight("123", new Pilot(true), new Pilot(true), List.of(), Language.ENGLISH);
+        Flight flight = new Flight("123", new Pilot(true), new Pilot(true), List.of(), ENGLISH);
         flight.isReadyForTakeOff();
 
         Flight flight2 = new Flight("1234", new Pilot(true), new Pilot(false), List.of(
-                new Attendant(),
-                new Attendant(),
-                new Attendant()
-        ), Language.ENGLISH);
+                new Attendant(Set.of(ENGLISH, GERMAN)),
+                new Attendant(Set.of(ENGLISH, GERMAN)),
+                new Attendant(Set.of(GERMAN))
+        ), ENGLISH);
         flight2.isReadyForTakeOff();
     }
 
--- src/main/java/org/example/airline/Attendant.java
+++ src/main/java/org/example/airline/Attendant.java
@@ -1,4 +1,13 @@
 package org.example.airline;
 
+import java.util.Set;
+
 public class Attendant {
+
+    private final Set<Language> languages;
+
+    public Attendant(Set<Language> languages) {
+        this.languages = Set.copyOf(languages);
+    }
+
 }
